249472284670f6d7a5d9564bc580e6eea9abfd21
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-05-27 08:43:48 +0200

    initial

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.less';
+
+ReactDOM.render(
+  'welcome', // react element would go here
+  document.querySelector('#target'),
+);
