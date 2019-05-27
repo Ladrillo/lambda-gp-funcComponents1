3790ab3fc167ab0f6ad871cb5ad4e8db6612dbff
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-05-27 11:50:06 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,9 @@ import './App.less';
 // this component takes two args: heading, and text.
 // invoke it the funny way to get an element
 // and attach said element to the DOM
-function MySection({ heading, text }) {
+function MySection({ heading, text, color }) {
   return (
-    <div>
+    <div style={{ backgroundColor: color }}>
       <h3>{heading}</h3>
       <p>{text}</p>
     </div>
@@ -20,16 +20,17 @@ function MySection({ heading, text }) {
 // a Container component that renders a couple of sections,
 // use it to create a container element and attach it to the DOM
 // Can you build me that???
-function Container({ }) {
+function Container({ color }) {
   return (
     <div>
-      <MySection heading="cats" text="miau" />
-      <MySection heading="dogs" text="bark" />
+      <h3>This is the Container</h3>
+      <MySection heading="cats" text="miau" color={color} />
+      <MySection heading="dogs" text="bark" color={color} />
     </div>
   );
 }
 
-const myContainerNotCapitalized = <Container heading="cats" text="foo" />;
+const myContainerNotCapitalized = <Container heading="cats" text="foo" color="grey" />;
 
 ReactDOM.render(
   myContainerNotCapitalized, // react element would go here
