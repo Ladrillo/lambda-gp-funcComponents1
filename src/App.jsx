c17d0a76f633bc000f5aa4a1919d07e7846cd740
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-05-27 11:33:36 +0200

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Car from './Car'; // not being used anymore
 import './App.less';
 
-// INVOCATION the funky invokation allows to nest components comfortably
-const someElement = <SomeElement id='main' className='nice' text='whatever' />;
 
-// DECLARATION (what's usually called a "component")
-function SomeElement({ className, id, text }) {
-  return (
-    <span id={id} className={className}>
-      {text}
-    </span>
-  );
-}
+// create a component that returnds a div
+// containing an h1 and a paragraph.
+// this component takes two args: heading, and text.
+// invoke it the funny way to get an element
+// and attach said element to the DOM
 
 ReactDOM.render(
   someElement, // react element would go here
